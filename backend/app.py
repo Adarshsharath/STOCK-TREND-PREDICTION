1c56156b0bcdba90888571a7dbd76322b00e5152
Adarshsharath <87135900+Adarshsharath@users.noreply.github.com>
2025-11-02 21:31:33 +0530

    Made debug = False in main code for deploying

--- backend/app.py
+++ backend/app.py
@@ -691,4 +691,4 @@ def get_simulator_data():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
